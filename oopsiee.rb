c819f6ecd6ca110e5b45c8ffb59fea9bb0aa2e13
Dinko Osrecki <dinko.osrecki@emarsys.com>
2022-03-08 11:20:42 +0100

    fix: oopsiee homebrew formula

--- oopsiee.rb
+++ oopsiee.rb
@@ -3,9 +3,7 @@ class Oopsiee < Formula
   homepage "https://github.com/edosrecki/oopsiee-cli"
   url "https://github.com/edosrecki/oopsiee-cli/releases/download/v1.5.1/oopsiee-macos-1.5.1.tar.gz"
   sha256 "087b2d3bd0e35123439f3709face71e2945653a741426dedeaf22ee045e50f3b"
-
-  bottle :unneeded
-
+  
   def install
     bin.install "oopsiee"
   end
