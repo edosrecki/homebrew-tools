bddb0c7852ebd0c625e6eba68edfdf95dbe7de60
Dinko Osrecki <dinko.osrecki@emarsys.com>
2026-01-12 10:51:40 +0000

    fix: unpublish google-cloud-sql v3

--- google-cloud-sql.rb
+++ google-cloud-sql.rb
@@ -1,9 +1,9 @@
 class GoogleCloudSql < Formula
   desc "Connect to private Google Cloud SQL/AlloyDB instance through Cloud SQL/AlloyDB Auth Proxy running in GKE cluster."
   homepage "https://github.com/edosrecki/google-cloud-sql-cli"
-  url "https://github.com/edosrecki/google-cloud-sql-cli/releases/download/v3.0.1/google-cloud-sql-macos-3.0.1.tar.gz"
-  sha256 "465ffa43e8f3d7c4b7b22e6c209de419f2acbcb80f313d579d2f35edb2d07a7b"
-  version "3.0.1"
+  url "https://github.com/edosrecki/google-cloud-sql-cli/releases/download/v2.0.0/google-cloud-sql-macos-2.0.0.tar.gz"
+  sha256 "b401bf70e53753cb2d01dfdc1d07bea676c77ac8f65d8675e95a5b0c2eb4272b"
+  version "2.0.0"
   def install
     bin.install "google-cloud-sql"
   end
